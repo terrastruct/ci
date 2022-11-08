c0ffcf0c9aea83aec3c84e05e769e88f1ee192f9
Anmol Sethi <anmol@terrastruct.com>
2022-11-08 15:21:07 -0800

    Add git.sh

--- lib.sh
+++ lib.sh
@@ -7,81 +7,8 @@ _LIB=1
 
 . "$(dirname "$0")/rand.sh"
 . "$(dirname "$0")/log.sh"
+. "$(dirname "$0")/git.sh"
 . "$(dirname "$0")/go.sh"
-
-# ***
-# git
-# ***
-
-set_git_base() {
-  if [ -n "${GIT_BASE_DONE:-}" ]; then
-    return
-  fi
-
-  if [ -n "${CI_ALL:-}" ]; then
-    return
-  fi
-
-  if git show --no-patch --format=%s%n%b | grep -qiF '\[ci-all\]'; then
-    return
-  fi
-
-  if [ "$(git rev-parse --is-shallow-repository)" = true ]; then
-    git fetch --unshallow origin master
-  fi
-
-  # Unfortunately --grep searches the whole commit message but we just want the header
-  # searched. Should fix by using grep directly later.
-  export GIT_BASE="$(git log --merges --grep="Merge pull request" --grep="\[ci-base\]" --format=%h HEAD~1 | head -n1)"
-  export GIT_BASE_DONE=1
-  if [ -n "$GIT_BASE" ]; then
-    echop make "GIT_BASE=$GIT_BASE"
-  fi
-}
-
-is_changed() {
-  set_git_base
-  if [ -z "${GIT_BASE:-}" ]; then
-    return
-  fi
-
-  ! git diff --quiet "$GIT_BASE" -- "$@" ||
-    [ -n "$(git ls-files --other --exclude-standard -- "$@")" ]
-}
-
-set_changed_files() {
-  set_git_base
-
-  filter_exists() {
-    while read -r p; do
-      if [ -e "$p" ]; then
-        printf '%s\n' "$p"
-      fi
-    done
-  }
-
-  if [ -n "${CHANGED_FILES:-}" ]; then
-    return
-  fi
-
-  CHANGED_FILES=./.changed-files
-  git ls-files --other --exclude-standard > "$CHANGED_FILES"
-  if [ -n "${GIT_BASE:-}" ]; then
-    git diff --relative --name-only "$GIT_BASE" | filter_exists >> "$CHANGED_FILES"
-  else
-    git ls-files >> "$CHANGED_FILES"
-  fi
-  export CHANGED_FILES
-}
-
-git_describe_ref() {
-  TAG="$(git describe 2> /dev/null || true)"
-  if [ -n "$TAG" ]; then
-    _echo "$TAG"
-  else
-    git rev-parse --short HEAD
-  fi
-}
 
 # ***
 # make
