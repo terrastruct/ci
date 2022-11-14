d064c179a5c102c4f54cf63be058461b21b8e28a
Anmol Sethi <anmol@terrastruct.com>
2022-11-13 21:57:40 -0800

    misc.sh: Add docker_run
    Closes #5

--- lib.sh
+++ lib.sh
@@ -625,6 +625,15 @@ aws() {
   # Without the redirection aws's cli will write directly to /dev/tty bypassing prefix.
   command aws "$@" > /dev/stdout
 }
+
+docker_run() {
+  sh_c docker run -it --rm \
+    -v "$HOME:$HOME" \
+    -w "$HOME" \
+    -e HOME \
+    -u "$(id -u):$(id -g)" \
+    "$@"
+}
 #!/bin/sh
 if [ "${LIB_NOTIFY-}" ]; then
   return 0
