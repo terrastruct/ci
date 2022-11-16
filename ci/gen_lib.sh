72caad4a0408dfbd262a5e1e08a17c743c9485fa
Anmol Sethi <anmol@terrastruct.com>
2022-11-16 10:51:27 -0800

    gen_lib.sh: Update

--- ci/gen_lib.sh
+++ ci/gen_lib.sh
@@ -17,7 +17,7 @@ find lib -name '*.sh' ! -name '*_test.sh' | sort | while read fname; do
   # Remove lines for sourcing dependency lib/*.sh files as all files are bundled into
   # lib.sh and so all dependencies will be satisfied. The individual files will not exist
   # when distributing just lib.sh and so sourcing will fail anyway.
-  sh_c sed '"/^\. /d"' "$fname" \>\> lib.sh
+  sh_c sed '"/^\. /d"' "$fname" \>\>lib.sh
 done
 sh_c chmod -w lib.sh
 
