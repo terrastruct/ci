de15341b9657e1001f658b8b36c9c4a883e3aaa4
Anmol Sethi <anmol@terrastruct.com>
2022-11-20 03:43:13 -0800

    make.sh: Disable $MAKE_LOG

--- lib/make.sh
+++ lib/make.sh
@@ -22,35 +22,32 @@ EOF
   if [ -z "${MAKE_LOG:-}" ]; then
     CI_MAKE_ROOT=1
     export MAKE_LOG="./.make-log"
-    set +e
-    if [ -t 1 ]; then
-      # runtty is necessary to allow make to write its output unbuffered. Otherwise the
-      # output is printed in surges as the write buffer is exceeded rather than a continous
-      # stream. Remove the runtty prefix to experience the laggy behaviour without it.
-      runtty make -sj8 "$@" \
-        | tee /dev/stderr "$MAKE_LOG" \
-        | stripansi > "$MAKE_LOG.txt"
-    else
-      make -sj8 "$@" \
-        | tee /dev/stderr "$MAKE_LOG" \
-        | stripansi > "$MAKE_LOG.txt"
-    fi
+    # set +e
+    # if [ -t 1 ]; then
+    #   # runtty is necessary to allow make to write its output unbuffered. Otherwise the
+    #   # output is printed in surges as the write buffer is exceeded rather than a continous
+    #   # stream. Remove the runtty prefix to experience the laggy behaviour without it.
+    #   runtty make -sj8 "$@" \
+    #     | tee /dev/stderr "$MAKE_LOG" \
+    #     | stripansi > "$MAKE_LOG.txt"
+    # else
+    #   make -sj8 "$@" \
+    #     | tee /dev/stderr "$MAKE_LOG" \
+    #     | stripansi > "$MAKE_LOG.txt"
+    # fi
   else
     CI_MAKE_ROOT=0
-    set +e
-    make -sj8 "$@" 2>&1
+    # set +e
+    # make -sj8 "$@" 2>&1
   fi
 
+  set +e
+  make -sj8 "$@" 2>&1
   code="$?"
   set -e
   if [ "$code" -ne 0 ]; then
     notify "$code"
     return "$code"
-  fi
-  # make doesn't return a nonsuccess exit code on recipe failures.
-  if <"$MAKE_LOG" grep -q 'make.* \*\*\* .* Error'; then
-    notify 1
-    return 1
   fi
   if [ -n "${CI:-}" ]; then
     # Make sure nothing has changed
