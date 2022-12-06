935d91b6229e978a3cf86c45394af37db4fa1232
Anmol Sethi <anmol@terrastruct.com>
2022-12-06 05:00:09 -0800

    temp.sh: Back to base64
    base32 isn't installed on macOS by default...

--- lib.sh
+++ lib.sh
@@ -1168,7 +1168,7 @@ temp_exittrap() {
 temppath() {
   ensure_tmpdir
   while true; do
-    temppath=$_TMPDIR/$(</dev/urandom head -c8 | base32)
+    temppath=$_TMPDIR/$(</dev/urandom head -c8 | base64 | tr / %)
     if [ ! -e "$temppath" ]; then
       echo "$temppath"
       return
